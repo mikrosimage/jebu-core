6fa7cfe162344ab1f0767e3967c2ec835cc74975
Guillaume Maucomble <gma@mikrosimage.eu>
2016-07-15 16:43:27 +0200

    Start IUT-R BS.2094-0 package

---
+++ src/main/java/itu/bs2094/adm/commons/package-info.java
@@ -0,0 +1,31 @@
+/**
+ * Following Rec.  ITU-R  BS.2094-0.
+ * 
+ * RECOMMENDATION  ITU-R  BS.2094-0
+ * Common definitions for the audio definition model (2016)
+ * <dl>
+ * <dt>Scope</dt>
+ * <dd>This Recommendation contains a set of common definitions for multichannel audio configurations that use the audio definition model (Recommendation ITU-R BS.2076) to describe them.</dt>
+ * </dl>
+ * <dl>
+ * <dt>Keywords</dt>
+ * <dd>ADM, audio, multichannel, channel-based, tracks, metadata, bw64, exchange, audio programme, BWF, immersive.
+ * The ITU Radiocommunication Assembly, considering :
+ * <ol>
+ * <li>that storage media based on information technology, including data disks and tapes, have penetrated all areas of audio production for radio broadcasting, namely non-linear editing, on‑air play-out and archives;</li>
+ * <li>that the adoption of a single file format for signal interchange would greatly simplify the interoperation of individual pieces of equipment, and remote studios, and it would facilitate the desirable integration of editing, on-air play-out, and archiving;</li>
+ * <li>that compatibility with currently-available commercial file formats would minimize the industry efforts required to implement a new format in the equipment;</li>
+ * <li>that future audio systems will require metadata associated with the audio to be carried in the file;</li>
+ * <li>that future audio systems will use a variety of multichannel configurations including channel‑, object-, and scene-based audio such as specified in Recommendation ITU-R BS.2051;</li>
+ * <li>that future audio systems will use the audio definition model (Recommendation ITU-R BS.2076) to describe the technical format of the audio being delivered and exchanged;</li>
+ * <li>that the majority of audio in existence and produced in the short-term future is and will be channel-based using a commonly used set of configurations;</li>
+ * <li>that using different metadata descriptions for identical audio configurations will cause compatibility problems and unnecessary overheads,
+recommends that, for exchanging any audio that uses the audio definition model (Recommendation ITU-R BS.2076) as the metadata model, a set of common definitions be used to describe any audio configurations as defined in Annex 1.</li>
+ *</ol>
+ * </dd>
+ * 
+ * 
+ * @author Guillaume Maucomble <gma@mikrosimage.eu>
+ *
+ */
+package itu.bs2094.adm.commons;
